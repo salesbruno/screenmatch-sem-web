4858323296eaef29993d1b6de30b522b48b7206d
Bruno Sales <salesdasilvabruno@gmail.com>
2025-06-10 11:41:38 -0300

    feat: created year filter

--- src/main/java/br/com/alura/screenmatch/principal/Principal.java
+++ src/main/java/br/com/alura/screenmatch/principal/Principal.java
@@ -8,6 +8,7 @@ import br.com.alura.screenmatch.services.ConsumoApi;
 import br.com.alura.screenmatch.services.ConverteDados;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -62,6 +63,20 @@ public class Principal {
 
         episodios.forEach(System.out::println);
 
+        System.out.println("A partir de que ano você deseja ver os episódios? ");
+        var ano = leitura.nextInt();
+        leitura.nextLine();
 
+        LocalDate dataBusca = LocalDate.of(ano, 1, 1);
+
+        DateTimeFormatter formatador = DateTimeFormatter.ofPattern("dd/MM/yyyy");
+
+        episodios.stream()
+                .filter(e -> e.getDataLancamento() != null && e.getDataLancamento().isAfter(dataBusca))
+                .forEach(e -> System.out.println(
+                        "Temporada:  " + e.getTemporada() +
+                                " Episodio: " +  e.getTitulo() +
+                                " Data Lançamento: " + e.getDataLancamento().format(formatador)
+                ));
     }
 }
